9d8f3f81a0e9efabafb163b7dca35db8ea5b6555
Andres Campuzano <andres.campuzano@cobistopaz.com>
2025-11-12 15:58:33 -0500

    inicio seccion 14

---
+++ 04-teslo-shop/src/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { inter } from "@/config/fonts";
+import "./globals.css";
+
+
+
+
+export const metadata: Metadata = {
+  title: "Teslo  | Shop",
+  description: "Una tienda digital de productos",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.className} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
